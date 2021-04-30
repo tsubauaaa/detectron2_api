fc79d8a4ca5102827feb5665824a2cb9889db630
tsubauaaa <tsubasa1173@gmail.com>
2021-04-30 14:03:03 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import base64
+from io import BytesIO
+
+import cv2
+import numpy as np
+import uvicorn
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import VisImage, Visualizer
+from fastapi import FastAPI, File, Request, UploadFile
+from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
+from PIL import Image, ImageDraw
+from pydantic import BaseModel
+from starlette.responses import FileResponse, JSONResponse, StreamingResponse
+from werkzeug.utils import secure_filename
+
+app = FastAPI()
+
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(
+    model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml")
+)
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"
+)
+
+cfg.MODEL.DEVICE = "cpu"
+
+
+class Data(BaseModel):
+    image: bytes
+
+
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.post("/predict")
+async def predict(data: Data):
+    # decode to image
+    # decimg = base64.b64decode(img_str.split(",")[1], validate=True)
+    decimg = base64.b64decode(data.image, validate=True)
+    decimg = Image.open(BytesIO(decimg))
+    decimg = np.array(decimg, dtype=np.uint8)
+    decimg = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
+    resize_decimg = cv2.resize(decimg, (300, 224))
+    predictor = DefaultPredictor(cfg)
+    outputs: dict = predictor(resize_decimg)
+
+    v = Visualizer(resize_decimg, MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out: VisImage = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    # cv2.imwrite("out.jpg", out.get_image())
+
+    # content = jsonable_encoder({"out": "out"})
+    # return JSONResponse(content=content)
+    _, out_jpg = cv2.imencode(".jpg", out.get_image())
+    return StreamingResponse(BytesIO(out_jpg.tobytes()), media_type="image/jpeg")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
---
+++ webcam_run.py
@@ -0,0 +1,76 @@
+import base64
+
+import cv2
+import numpy as np
+import requests
+
+profile = "developer"
+endPoint = "sampleEndPoint"
+categories = ["Bisco", "BlackThunder", "Alfort"]
+
+deviceId = 0  # Webカメラのデバイスインデックス
+height = 600
+width = 800
+linewidth = 2
+colors = [(0, 0, 175), (175, 0, 0), (0, 175, 0)]
+
+
+cap = cv2.VideoCapture(deviceId)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print("FPS:{}　WIDTH:{}　HEIGHT:{}".format(fps, width, height))
+
+while True:
+
+    # カメラ画像取得
+    ret, frame = cap.read()
+    if frame is None:
+        continue
+
+    _, jpg = cv2.imencode(".jpg", frame)
+    encimg = base64.b64encode(jpg)
+    encimg_str = encimg.decode("utf-8")
+    payload = {"image": encimg_str}
+
+    detections = requests.post(
+        "http://0.0.0.0:8001/predict",
+        json=payload,
+        headers={"Content-Type": "application/json"},
+    )
+    nparr = np.frombuffer(detections.content, np.uint8)
+
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
+    # for detection in detections["prediction"]:
+    #     clsId = int(detection[0])
+    #     confidence = detection[1]
+    #     x1 = int(detection[2] * width)
+    #     y1 = int(detection[3] * height)
+    #     x2 = int(detection[4] * width)
+    #     y2 = int(detection[5] * height)
+    #     label = "{} {:.2f}".format(categories[clsId], confidence)
+    #     if confidence > 0.6:  # 信頼度
+    #         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), colors[clsId], linewidth)
+    #         frame = cv2.rectangle(
+    #             frame, (x1, y1), (x1 + 150, y1 - 20), colors[clsId], -1
+    #         )
+    #         cv2.putText(
+    #             frame,
+    #             label,
+    #             (x1 + 2, y1 - 2),
+    #             cv2.FONT_HERSHEY_SIMPLEX,
+    #             0.5,
+    #             (255, 255, 255),
+    #             1,
+    #             cv2.LINE_AA,
+    #         )
+
+    cv2.imshow("frame", img_np)
+    cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
